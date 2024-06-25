868090e8ca96ca791a00fca127b29e7fcdb332e4
yuruyu6 <yuruyu6@gmail.com>
2024-06-25 17:44:32 +0300

    initial

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 90,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  darkMode: ["class"],
+  plugins: [require("tailwindcss-animate")],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
     },
   },
-  plugins: [],
-};
+} satisfies Config;
+
 export default config;
